f41ec4f9fb12c761e0515683eae8c67f71164aee
icebatish <65935218+icebatish@users.noreply.github.com>
2020-05-25 22:04:30 -0600

    im still broke @jnug

--- test.py
+++ test.py
@@ -3,8 +3,8 @@ from alpha_vantage.timeseries import TimeSeries
 import matplotlib.pyplot as plt
 
 ts = TimeSeries(key='G9V53KVNRI8KMSXH', output_format='pandas')
-data, meta_data = ts.get_daily(symbol='AAPL')
+data, meta_data = ts.get_daily(symbol='JNUG')
 print(data)
 data['4. close'].plot()
-plt.title('AAPL')
+plt.title('JNUG')
 plt.show()
