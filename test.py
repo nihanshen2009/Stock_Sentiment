19578fb6aa0315bb894f32f873fe778ad3306f39
hanshenni <hanshen.ni@rheem.com>
2020-11-20 03:19:05 -0600

    added google trends data imports
    added google trends,
    needs more work on showing trends data

--- test.py
+++ test.py
@@ -1,17 +1,33 @@
 #test.py
 from alpha_vantage.timeseries import TimeSeries
 from newsapi import NewsApiClient
+from pytrends.request import TrendReq
+
 import matplotlib.pyplot as plt
 
-#price data
+"""
+google trends data
+"""
+pytrends = TrendReq(hl='en-US', tz=360)
+kw_list = ["walmart stock"]
+pytrends.build_payload(kw_list, cat=0, timeframe='today 5-y', geo='GLOBAL', gprop='')
+
+interest_over_time_df = pytrends.interest_over_time()
+print(interest_over_time_df)
+
+"""
+price data
+"""
 ts = TimeSeries(key='G9V53KVNRI8KMSXH', output_format='pandas')
 wmt_price, meta_data = ts.get_daily(symbol='WMT', outputsize='full') #get all historic prices
 #print(wmt_price)
 wmt_price['4. close'].plot()
 plt.title('Walmart ')
-#plt.show()
 
-#news data
+
+"""
+news data
+"""
 newsapi = NewsApiClient(api_key='ff0f41a5f9804ed69aa4e250e0ae9177')
 news_data = newsapi.get_everything(q='walmart',
                                     sources='bloomberg, the-washington-post, the-wall-street-journal', 
@@ -33,9 +49,10 @@ print(len(variable_name))
 for i in range(len(variable_name)):
     title = variable_name[i]
     print(title['title'])
-=======
-data, meta_data = ts.get_daily(symbol='JNUG')
-print(data)
-data['4. close'].plot()
-plt.title('JNUG')
+
 plt.show()
+#data, meta_data = ts.get_daily(symbol='JNUG')
+#print(data)
+#data['4. close'].plot()
+#plt.title('JNUG')
+#plt.show()
