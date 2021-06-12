77325dc7c90faf8fcd78028caeef1119e9b16b93
hanshenni <nihanshen2009@gmail.com>
2021-06-12 02:17:44 -0500

    Softmax Activation Function

--- ml1.py
+++ ml1.py
@@ -1,17 +1,17 @@
+"""
+A simple neural network from scratch
+
+inspired by sentdex from youtube
+
+"""
+
+
 import numpy as np
 import nnfs
-from nnfs.datasets import spiral_data
+from nnfs.datasets import spiral, spiral_data
 
 
 nnfs.init()
-
-# a layer of neurons
-X = [[1, 2, 3, 2.5],
-    [2.0, 5.0, -1.0, 2.0],
-    [-1.5, 2.7, 3.3, -0.8]]
-
-X, y = spiral_data(100, 3)
-
 
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
@@ -21,14 +21,35 @@ class Layer_Dense:
         self.output = np.dot(inputs, self.weights) + self.biases
 
 class Activation_ReLU:
+    """
+    Rectified Linear Activation Function
+
+    """
     def forward(self, inputs):
         self.output = np.maximum(0, inputs)
 
-layer1 = Layer_Dense(2,5)
+class Activation_Softmax:
+    """
+    Softmax Activation Function
+    """
+    def forward(self,inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
+
+
+X, y = spiral_data(samples=100, classes=3)
+
+dense1 = Layer_Dense(2,3)
 activation1 = Activation_ReLU()
 
+dense2 = Layer_Dense(3, 3)
+activation2 = Activation_Softmax()
 
-layer1.forward(X)
-activation1.forward(layer1.output)
+dense1.forward(X)
+activation1.forward(dense1.output)
 
-print(activation1.output)
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+
+print(activation2.output[:5])
